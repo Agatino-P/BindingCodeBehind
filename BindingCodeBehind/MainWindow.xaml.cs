f897879ab33092b735c4b9296d1bf7fdf5a6832c
Agatino Pesce <a.pesce@mecal.com>
2020-12-15 10:12:09 +0100

    Add project files.

---
+++ BindingCodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingCodeBehind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox newTextBlock = new TextBox();
+            newTextBlock.Text="just added";
+            
+            
+            newTextBlock.SetBinding(TextBox.TextProperty, new Binding("Testo") { Source = DataContext as MainWindowViewModel });
+            stackP.Children.Add(newTextBlock);
+        }
+    }
+}
---
+++ BindingCodeBehind/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BindingCodeBehind
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _testo; public string Testo { get => _testo; set { Set(() => Testo, ref _testo, value); }}
+    }
+}
